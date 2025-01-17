2e8d60f4abbfc46b12375e7a4060d518be39a155
ahmad ghasan <sotaalshamary@gmail.com>
2025-01-18 01:12:00 +0300

    Update userInfo

--- FrontEnd/src/presentation/components/profile/UserInfo.tsx
+++ FrontEnd/src/presentation/components/profile/UserInfo.tsx
@@ -5,30 +5,30 @@ import UserRoadmaps from "./UserRoadmaps";
 const UserInfo = () => {
   const { user } = useAuthStore(); // Ensure `user` exists
 
-  if (!user) {
-    console.warn(
-      "User data is not available. Ensure the store is correctly initialized."
-    );
-    return null;
-  }
+  // if (!user) {
+  //   console.warn(
+  //     "User data is not available. Ensure the store is correctly initialized."
+  //   );
+  //   return null;
+  // }
 
   const stats = [
     {
       icon: Star,
       label: "Selected Paths",
-      value: user.selectedRoadmaps?.length || 0,
+      value: user?.selectedRoadmaps?.length || 0,
       color: "text-yellow-500",
     },
     {
       icon: Clock,
       label: "User Skills",
-      value: user.selectedSkills?.length || 0,
+      value: user?.selectedSkills?.length || 0,
       color: "text-blue-500",
     },
     {
       icon: Trophy,
       label: "Completed Tasks",
-      value: Object.values(user.progress || {}).flat().length,
+      value: Object.values(user?.progress || {}).flat().length,
       color: "text-green-500",
     },
   ];
@@ -36,9 +36,8 @@ const UserInfo = () => {
   return (
     <div className="bg-gradient-to-r from-blue-500/10 to-purple-500/10 rounded-2xl p-8 mb-12">
       <h1 className="text-4xl font-bold mb-4 text-theme">
-        Welcome, {user.first_name} {user.last_name}
+        Welcome, {user?.first_name} {user?.last_name}
       </h1>
-
       {/* Stats Section */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8">
         {stats.map((stat, index) => (
@@ -60,7 +59,6 @@ const UserInfo = () => {
           </div>
         ))}
       </div>
-
       <div className="mt-6">
         {user?.selectedRoadmaps?.length > 0 ? (
           <UserRoadmaps />
